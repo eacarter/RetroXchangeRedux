ebec5e281da564c548988906200458d138727918
eacarter@mtu.edu <flamefire1>
2020-06-05 15:47:23 -0400

    Initial Commit

---
+++ RetroX_Kotlin/app/src/main/java/com/erickson/retroxchange/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.erickson.retroxchange.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
