29a50cfc3d5536a163cb62c56faa60f5f471e12e
eacarter@mtu.edu <flamefire1>
2020-06-14 19:46:45 -0400

    Working on profile send and receive data

--- RetroX_Kotlin/app/src/main/java/com/erickson/retroxchange/MainActivity.kt
+++ RetroX_Kotlin/app/src/main/java/com/erickson/retroxchange/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : DaggerAppCompatActivity(), HasSupportFragmentInjector {
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_notifications ))
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
         mainViewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
